16e682abc2d2868fef7271b22e8c0f1fc04d7816
brunavieirat <bruna.vieira.t@hotmail.com>
2018-05-02 23:20:37 -0300

    configuration

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Navigation } from 'react-native-navigation'
+
+
+export default() =>{
+
+//Navigation.registerComponent('Login', () => Login);
+
+// Navigation.startSingleScreenApp({
+//     screen: {
+//     screen: 'Login',
+//     title: 'Login'
+//     }
+// })
+}
